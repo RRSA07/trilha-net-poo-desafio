d40768568050cdf577d2a8f8404500bfda46a22b
RRSA07 <rafael.rodrigues07@gmail.com>
2022-11-06 17:02:13 -0300

    Desafio 3 Potencial

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Instanciar as classes Nokia e Iphone
+Nokia smartphoneNokia = new Nokia("7199999-9999","Nokia N12","abcdefg123",32);
+Iphone smartphoneIphone = new Iphone("1199999-9999","Iphone 11","hlmnop987",64);
+//Usar os métodos da classe Nokia
+smartphoneNokia.Ligar();
+smartphoneNokia.ReceberLigacao();
+smartphoneNokia.InstalarAplicativo("Telegram");
+//Usar os métodos da classe Iphone
+smartphoneIphone.Ligar();
+smartphoneIphone.ReceberLigacao();
+smartphoneIphone.InstalarAplicativo("WhatsApp");
